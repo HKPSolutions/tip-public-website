914c3bc42298824e5c752a37fd86ee31498c7b5c
Joshua Lamke <joshua.lamke@gmail.com>
2021-01-08 18:45:00 -0500

    finished sign up page body

--- src/components/pages/signup/Signup.jsx
+++ src/components/pages/signup/Signup.jsx
@@ -2,8 +2,19 @@ import './atom.css'
 
 function SignUpPage() {
     return (
-        <div>
-            [SignUpPage]
+        <div id="main-container">
+            <div id="header">
+                <h1 id="title">Contact Us</h1>
+                <div id="header-info-container">
+                    <p id="header-info">Please feel free to contact us at info@tipsolutions.com or fill out the below contact form if you have any questions.</p>
+                </div>
+            </div>
+            <form action="" id="message-form">
+                <input type="text" placeholder="Full Name"/>
+                <input type="text" placeholder="Email"/>
+                <textarea name="" id="" cols="15" rows="100" placeholder="Message"></textarea>
+                <input type="submit" value="Submit" id="submit-button"/>
+            </form>
         </div>
     );
 }
