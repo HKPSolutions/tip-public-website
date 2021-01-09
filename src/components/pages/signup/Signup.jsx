06d246dd2beb15f62ff5af8a72a9174b7fbd717c
Joshua Lamke <joshua.lamke@gmail.com>
2021-01-08 19:42:26 -0500

    changed css id's and classNames

--- src/components/pages/signup/Signup.jsx
+++ src/components/pages/signup/Signup.jsx
@@ -2,18 +2,18 @@ import './atom.css'
 
 function SignUpPage() {
     return (
-        <div id="main-container">
-            <div id="header">
-                <h1 id="title">Contact Us</h1>
-                <div id="header-info-container">
-                    <p id="header-info">Please feel free to contact us at info@tipsolutions.com or fill out the below contact form if you have any questions.</p>
+        <div id="signup-main-container">
+            <div id="signup-header">
+                <h1 id="signup-title">Contact Us</h1>
+                <div id="signup-header-info-container">
+                    <p id="signup-header-info">Please feel free to contact us at info@tipsolutions.com or fill out the below contact form if you have any questions.</p>
                 </div>
             </div>
-            <form action="" id="message-form">
-                <input type="text" placeholder="Full Name"/>
-                <input type="text" placeholder="Email"/>
-                <textarea name="" id="" cols="15" rows="100" placeholder="Message"></textarea>
-                <input type="submit" value="Submit" id="submit-button"/>
+            <form action="" id="signup-message-form">
+                <input type="text" placeholder="Full Name" className="signup-input"/>
+                <input type="text" placeholder="Email"  className="signup-input"/>
+                <textarea name="" id="" cols="15" rows="100" placeholder="Message"  className="signup-textarea"></textarea>
+                <input type="submit" value="Submit" id="signup-submit-button"/>
             </form>
         </div>
     );
