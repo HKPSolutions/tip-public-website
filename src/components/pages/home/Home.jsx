d84b300bcc32a0887116fd7ae05b7fb35dd952bd
johnjamespj <johnjamespj@gmail.com>
2021-01-10 17:43:54 -0500

    minor fixes

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -7,12 +7,12 @@ function HomePage() {
     return (
         <div id="home-container">
             <h3 id="home-main-brand-name">Training and Internship Placement Solutions</h3>
-            <h3 id="home-main-tagline">Request a 13-Week Remote Software Engineering Intern </h3>
+            <h3 id="home-main-tagline">Request a 14-Week Remote Software Engineering Intern </h3>
             <h4 id="home-sub-tagline">No additional payment to interns required</h4>
             <h4 id="home-deadline-tagline">Request an intern by February 12th for Spring 2021</h4>
             <hr id="home-separator"></hr>
             <p id="pricing-tagline">One-time $1,000 recruitment fee per intern</p>
-            <button id="home-request-intern-btn"><Link to="/signup" className="text-white no-link-btn">Click here to request an intern!</Link></button>
+            <button id="home-request-intern-btn"><a href="https://forms.gle/Cvb1NzKALvT2QoU36"  className="text-white no-link-btn">Click here to request an intern!</a></button>
             <div id="home-center-content">
                 <img src={HomeBg} alt="Black wave background" id="home-bg-img"></img>
                 <div id="home-center-container">
@@ -29,7 +29,7 @@ function HomePage() {
                         <li>We have trained over <strong>100 interns to-date</strong></li>
                     </ul>
                 </div>
-                <button id="home-get-started-btn"><a href="https://forms.gle/Cvb1NzKALvT2QoU36" className="text-black no-link-btn">Get Started Today!</a></button>
+                <button id="home-get-started-btn"><Link to="/signup" className="text-black no-link-btn">Get Started Today!</Link></button>
             </div>
         </div>
     );
