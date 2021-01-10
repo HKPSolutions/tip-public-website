938d04f7866b1569fe154399ef9b08228dbebbd5
johnjamespj <johnjamespj@gmail.com>
2021-01-10 10:47:59 -0500

    implemented emailjs

--- src/components/pages/signup/Signup.jsx
+++ src/components/pages/signup/Signup.jsx
@@ -1,6 +1,33 @@
 import './atom.css'
+import emailjs from 'emailjs-com'
+import { useState } from 'react';
+
+const ERROR = "Something Went Wrong!"
+const LOADING = "Loading..."
+const SEND = "Message Sent! We will be in touch with you soon!"
 
 function SignUpPage() {
+    const [alert, setAlert] = useState(null);
+
+    const onSubmit = (e) => {
+        setAlert(LOADING)
+        emailjs.sendForm(process.env.REACT_APP_serviceID, process.env.REACT_APP_templateID, e.target, process.env.REACT_APP_userID)
+            .then((result) => {
+                setAlert(SEND)
+                console.log(result.text);
+                setTimeout(() => {
+                    setAlert(null)
+                }, 3000)
+            }, (error) => {
+                setAlert(ERROR)
+                console.log(error.text);
+                setTimeout(() => {
+                    setAlert(null)
+                }, 3000)
+            });
+        e.preventDefault();
+    }
+
     return (
         <div id="signup-main-container">
             <div id="signup-header">
@@ -9,11 +36,14 @@ function SignUpPage() {
                     <p id="signup-header-info">Please feel free to contact us at info@tipsolutions.com or fill out the below contact form if you have any questions.</p>
                 </div>
             </div>
-            <form action="" id="signup-message-form">
-                <input type="text" placeholder="Full Name" className="signup-input"/>
-                <input type="text" placeholder="Email"  className="signup-input"/>
-                <textarea name="" id="" cols="15" rows="100" placeholder="Message"  className="signup-textarea"></textarea>
-                <input type="submit" value="Submit" id="signup-submit-button"/>
+            <form onSubmit={onSubmit} id="signup-message-form">
+                <input name="full_name" type="text" placeholder="Full Name" className="signup-input" />
+                <input name="email" type="text" placeholder="Email" className="signup-input" />
+                <textarea name="message" id="" cols="15" rows="100" placeholder="Message" className="signup-textarea"></textarea>
+                {alert && <div className="alert" style={{
+                    backgroundColor: alert === LOADING ? 'blue' : alert === ERROR ? 'red' : 'green'
+                }}>{alert}</div>}
+                <input type="submit" value="Submit" id="signup-submit-button" />
             </form>
         </div>
     );
